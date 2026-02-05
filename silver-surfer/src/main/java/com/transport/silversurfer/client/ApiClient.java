f2cce60b359a725306c7dab2cb7b38832d0d5991
Ashlyn Buckley <ashlynbuckleyemail3@gmail.com>
2026-02-05 22:23:24 +0000

    can print json response

--- silver-surfer/src/main/java/com/transport/silversurfer/client/ApiClient.java
+++ silver-surfer/src/main/java/com/transport/silversurfer/client/ApiClient.java
@@ -2,6 +2,7 @@ package com.transport.silversurfer.client;
 
 import org.apache.hc.client5.http.entity.UrlEncodedFormEntity;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.core5.http.ClassicHttpRequest;
 import org.apache.hc.core5.http.HttpEntity;
@@ -12,32 +13,30 @@ import org.apache.hc.core5.http.message.BasicNameValuePair;
 // Handles HTTP requests to NTA's GTFS-Realtime API
 // https://developer.nationaltransport.ie/api-details#api=gtfsr&operation=gtfsr-v2
 public class ApiClient {
+    String apiUrl = "https://api.silversurfer.com";
 
     public void createHttpClient() {
-        //putting it in a try-with-resources to ensure the resource is closed
+        // Putting it in a try-with-resources to ensure the resource is closed
+        // This CloseableHttpClient will be the reusable client I use to send requests
         try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+            // Builder for GET reqs
             ClassicHttpRequest httpGet = ClassicRequestBuilder.get("http://httpbin.org/get")
                     .build();
 
+            // using a lambda means the response will be closed after the lambda finishes
             httpclient.execute(httpGet, response -> {
                 System.out.println(response.getCode() + " " + response.getReasonPhrase());
-                final HttpEntity entity1 = response.getEntity();
-                EntityUtils.consume(entity1);
+                final HttpEntity entity = response.getEntity();
+                if (entity != null) {
+                            // Convert the entity to a string (JSON text)
+                            String json = EntityUtils.toString(entity);
+                            System.out.println("Response JSON:");
+                            System.out.println(json);
+                }
+                // closes entity
+                EntityUtils.consume(entity);
                 return null;
             });
-
-//            ClassicHttpRequest httpPost = ClassicRequestBuilder.post("http://httpbin.org/post")
-//                    .setEntity(new UrlEncodedFormEntity(Arrays.asList(
-//                            new BasicNameValuePair("username", "vip"),
-//                            new BasicNameValuePair("password", "secret"))))
-//                    .build();
-//            httpclient.execute(httpPost, response -> {
-//                System.out.println(response.getCode() + " " + response.getReasonPhrase());
-//                final HttpEntity entity2 = response.getEntity();
-//                EntityUtils.consume(entity2);
-//                return null;
-//            });
-
         } catch (Exception e) {
             e.printStackTrace();
         }
