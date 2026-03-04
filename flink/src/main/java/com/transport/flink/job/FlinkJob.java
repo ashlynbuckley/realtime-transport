e415d7b78445e26ac0ec9a60d75b00655cc5f9da
Ashlyn Buckley <ashlynbuckleyemail3@gmail.com>
2026-03-04 21:49:12 +0000

    flink can take in from kafka

--- flink/src/main/java/com/transport/flink/job/FlinkJob.java
+++ flink/src/main/java/com/transport/flink/job/FlinkJob.java
@@ -1,6 +1,6 @@
 package com.transport.flink.job;
 
-import com.transport.flink.process.Bus;
+import com.fyp.avro.AvroVehicleEvent;
 import com.transport.flink.source.KafkaSourceFactory;
 import com.transport.flink.sink.MySQLSinkFactory;
 import org.apache.flink.streaming.api.datastream.DataStream;
@@ -14,14 +14,26 @@ public class FlinkJob {
 
         //Source
         // TODO: Duplicate records needs to be handled
-//        DataStream<Bus> busEvent = KafkaSourceFactory.createKafkaBusSource(env);
-        DataStream<String> kafkaStream = KafkaSourceFactory.createKafkaSource(env);
-        //Print what we ingested for debugging
-        kafkaStream.print();
+        DataStream<AvroVehicleEvent> kafkaStream = KafkaSourceFactory.createKafkaSource(env);
+        if (kafkaStream != null) {
+            System.out.println("Stream is not null");
+            parseRouteIds(kafkaStream);
+        }
+//        parseRouteIds(kafkaStream);
         //Sink
-        kafkaStream.addSink(MySQLSinkFactory.configureSink());
+//        kafkaStream.addSink(MySQLSinkFactory.configureSink());
 
         env.execute("Process Messages");
 
+    }
+
+    private static void parseRouteIds(DataStream<AvroVehicleEvent> kafkaStream) {
+        System.out.println("Parsing Route Ids");
+        //Note: event isn't giving back String, it gives CharSequence
+        DataStream<AvroVehicleEvent> parsedRoute = kafkaStream.filter(event -> {
+            CharSequence cs = event.getRouteId();
+            return cs != null && cs.toString().equals("5401_126001");
+        });
+        parsedRoute.print();
     }
 }
--- flink/src/main/java/com/transport/flink/source/KafkaSourceFactory.java
+++ flink/src/main/java/com/transport/flink/source/KafkaSourceFactory.java
@@ -1,22 +1,25 @@
 package com.transport.flink.source;
 
-import com.transport.flink.process.Bus;
+import com.fyp.avro.AvroVehicleEvent;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
-import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.formats.avro.registry.confluent.ConfluentRegistryAvroDeserializationSchema;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 public class KafkaSourceFactory {
 
-    public static DataStream<String> createKafkaSource(StreamExecutionEnvironment env) {
-        KafkaSource<String> source = KafkaSource.<String>builder()
-                .setBootstrapServers("localhost:9092") //kafka broker sending through here
+    public static DataStream<AvroVehicleEvent> createKafkaSource(StreamExecutionEnvironment env) {
+        KafkaSource<AvroVehicleEvent> source = KafkaSource.<AvroVehicleEvent>builder()
+                .setBootstrapServers("localhost:29092") //kafka broker sending through here
                 .setTopics("test-topic-sb")
                 .setGroupId("flink-test-group")
                 .setStartingOffsets(OffsetsInitializer.earliest())
-                .setValueOnlyDeserializer(new SimpleStringSchema())
+                .setValueOnlyDeserializer(
+                        ConfluentRegistryAvroDeserializationSchema
+                                .forSpecific(AvroVehicleEvent.class, "http://localhost:8081")
+                )
                 .build();
 
         return env.fromSource(
@@ -25,20 +28,4 @@ public class KafkaSourceFactory {
                 "Kafka Source"
         );
     }
-
-//    public static DataStream<Bus> createKafkaBusSource(StreamExecutionEnvironment env) {
-//        KafkaSource<String> source = KafkaSource.<String>builder()
-//                .setBootstrapServers("localhost:9092") //kafka broker sending through here
-//                .setTopics("test-topic-sb")
-//                .setGroupId("flink-test-group")
-//                .setStartingOffsets(OffsetsInitializer.earliest())
-//                .setValueOnlyDeserializer(new SimpleStringSchema())
-//                .build();
-//
-//        return env.fromSource(
-//                source,
-//                WatermarkStrategy.noWatermarks(),
-//                "Kafka Source"
-//        );
-//    }
 }
